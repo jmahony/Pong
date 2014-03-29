eb099e1e21f0a6d47eda5648268fad0ad2248e7e
Josh Mahony <jalmahony@gmail.com>
2014-03-29 14:35:40 +0000

    Add in more comments

--- server/S_PongModel.java
+++ server/S_PongModel.java
@@ -11,14 +11,46 @@ import java.util.Observable;
  * The active object ActiveModel does the work of moving the ball
  */
 public class S_PongModel extends Observable {
+
+    /**
+     * The games ball
+     */
     private GameObject ball = new GameObject(W / 2, H / 2, BALL_SIZE, BALL_SIZE);
+
+    /**
+     * The players bats
+     */
     private GameObject bats[] = new GameObject[2];
 
+    /**
+     * The active model of the game, this is the part of the game that keeps the
+     * game movingforward
+     */
     private Thread activeModel;
 
+    /**
+     * The last ping timestamp received from the players,
+     * left is at position 0 and right is at position 1
+     */
     private long[] lastPingTimestamp = new long[2];
+
+    /**
+     * The average pings for both players
+     */
     private long[] averagePings = new long[2];
+
+    /**
+     * The last request round trip time, this isn't actually used by anything
+     * but I thought i'd leave it in in case it becomes useful in the next
+     * version
+     */
     private long[] lastRequestRTT = new long[2];
+
+    /**
+     * The delay that was applied to the last update of the players game,
+     * this is needed so we can estimate their real ping on the next tick.
+     */
+    public long[] lastUpdateDelay = new long[2];
 
     public S_PongModel() {
         bats[0] = new GameObject(60, H / 2, BAT_WIDTH, BAT_HEIGHT);
@@ -120,14 +152,63 @@ public class S_PongModel extends Observable {
      * @param playerId
      * @param roundTripTime
      */
-    public void setLastRequestRTT(int playerId, long roundTripTime) { lastRequestRTT[playerId] = roundTripTime; }
+    public void setLastRequestRTT(int playerId, long roundTripTime) {
+        lastRequestRTT[playerId] = roundTripTime;
+    }
 
     /**
      *
      * @param playerId
      * @return
      */
-    public long getLastRequestRTT(int playerId) { return lastRequestRTT[playerId]; }
+    public long getLastRequestRTT(int playerId) {
+        return lastRequestRTT[playerId];
+    }
+
+    public long getLastUpdateDelay(int playerId) {
+        return lastUpdateDelay[playerId];
+    }
+
+    public void setLastUpdateDelay(int playerId, long delay) {
+        // Make sure we don't store any negative values
+        lastUpdateDelay[playerId] = delay >= 0 ? delay : 0;
+    }
+
+    public long[] getDelays() {
+
+        // Get both players average ping
+        // e.g. leftAvgPing = 200
+        //      rightAvgPing = 200
+        long leftAvgPing  = getAveragePing(Global.LEFT_PLAYER),
+             rightAvgPing = getAveragePing(Global.RIGHT_PLAYER);
+
+        // Calculate how much we need to delay each player.
+        // One players delay will always be negative
+        // so their update will be sent immediately
+        // e.g. leftDelay  = 200 - 200 = 0
+        //      rightDelay = 200 - 200 = 0
+        long leftDelay  = (rightAvgPing - leftAvgPing),
+             rightDelay = (leftAvgPing - rightAvgPing);
+
+        // Calculate the actual delay (including the last delay)
+        // e.g. Assuming the left player was delayed by 50ms last tick
+        //     delay = (0 + 50) * 2 = 50 = 100ms delay
+        //     delay = (0 + 0) * 2  = 0 = 0ms delay
+        //     Anything created than or equal to 0 will treated as 0.
+        long leftActualDelay  = leftDelay * 2 +
+                getLastUpdateDelay(Global.LEFT_PLAYER),
+
+             rightActualDelay = rightDelay * 2 +
+                getLastUpdateDelay(Global.RIGHT_PLAYER);
+
+        // Store how much we've delayed each player so we
+        // can offset it against their delay on the next tick
+        setLastUpdateDelay(Global.LEFT_PLAYER, leftDelay);
+        setLastUpdateDelay(Global.RIGHT_PLAYER, rightDelay);
+
+        return new long[] {leftActualDelay, rightActualDelay};
+
+    }
 
     /**
      * Cause update of view of game
@@ -137,6 +218,5 @@ public class S_PongModel extends Observable {
         setChanged();
         notifyObservers();
     }
-
 
 }
